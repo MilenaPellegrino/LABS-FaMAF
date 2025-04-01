d64b7bde5135af06782ec774de11cb5a09db163c
Juan Gonzalez <juanpgonzalez4002@mi.unc.edu.ar>
2025-04-01 17:26:22 -0300

    Avanzando en la implementación de esRot360

--- Pred.hs
+++ Pred.hs
@@ -1,4 +1,4 @@
-module Dibujo where
+module Pred where
 
 import Dibujo
 
@@ -23,9 +23,28 @@ allDib :: Pred a -> Dibujo a -> Bool
 
 -- Hay 4 rotaciones seguidas.
 esRot360 :: Pred (Dibujo a)
+esRot360 d = fst (foldDib checkRotations resetState id id (\_ _ x y -> max x y) (\_ _ x y -> max x y) max d)
+    where
+        --Estado inicial: (tupla con acumulador de rotaciones consecutivas con un flag de 4 rotaciones seguidas)
+        resetState = (0, False)
+
+        --Función para actualizar el estado
+        checkRotations :: a -> (Int, Bool) -> (Int, Bool)
+        checkRotations _ (count, _) 
+            | count >= 4 = (count, True)  -- Ya alcanzamos 4 rotaciones consecutivas
+            | otherwise = (count, False) 
+
+         -- Función para manejar rotaciones
+        rotf :: (Int, Bool) -> (Int, Bool)
+        rotf (count, _) = if count + 1 >= 4 then (4, True) else (count + 1, False)
+
+        -- Función para manejar otros tipos de dibujos, que reinicia el contador
+        idf :: (Int, Bool) -> (Int, Bool)
+        idf _ = (0, False)
 
 -- Hay 2 espejados seguidos.
 esFlip2 :: Pred (Dibujo a)
+
 
 data Superfluo = RotacionSuperflua | FlipSuperfluo
 
@@ -38,5 +57,3 @@ errorFlip :: Dibujo a -> [Superfluo]
 -- Aplica todos los chequeos y acumula todos los errores, y
 -- sólo devuelve la figura si no hubo ningún error.
 checkSuperfluo :: Dibujo a -> Either [Superfluo] (Dibujo a)
-
-
