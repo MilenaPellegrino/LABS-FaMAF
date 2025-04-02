428ab3e31bc71b91b99fa4822cd7fe26ed92f4c5
MilenaPellegrino <milenapellegrinoch@gmail.com>
2025-04-02 18:10:59 -0300

    Inter de apilar + modificacion de espejar

--- Interp.hs
+++ Interp.hs
@@ -22,19 +22,21 @@ mitad = (0.5 V.*)
 
 --interpreta el operador de rotacion
 interp_rotar :: ImagenFlotante -> ImagenFlotante
-interp_rotar f = \d w h -> f (d V.+ w) h (zero V.- w)
+interp_rotar f = \d w h -> f (d V.+ w) h (V.negate w)
 
 
 --interpreta el operador de espejar
 interp_espejar :: ImagenFlotante -> ImagenFlotante
-interp_espejar f = \d w h -> f d (zero V.- w) h
+interp_espejar f = \d w h -> f (d V.+ w) (V.negate w) h
 
 --interpreta el operador de rotacion 45
 interp_rotar45 :: ImagenFlotante -> ImagenFlotante
 interp_rotar45 f = \d w h -> f (d V.+ mitad (w V.+ h)) (mitad (w V.+ h)) (mitad (h V.- w))
 
--- --interpreta el operador de apilar
--- interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+--interpreta el operador de apilar
+-- Veri si esta bien porque me da error porque no tengo la de encimar implementada
+interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_apilar m n f1 f2 d w h = interp_encimar (f (d V.+ ((n/(m+n)) V.* h) ) w (m/(m+n) V.* h)) (g d w (n/(m+n) V.* h))
 
 -- --interpreta el operador de juntar
 -- interp_juntar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
