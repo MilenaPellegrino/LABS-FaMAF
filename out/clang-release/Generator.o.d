52f4c26e658e85d049411be98161424995a3b363
Ismadero <ismaelmadero2003@gmail.com>
2025-05-13 12:21:56 -0300

    fix: Borrar binarios

--- out/clang-release/Generator.o.d
+++
@@ -1,3 +0,0 @@
-out/clang-release//Generator.o: Generator.cc \
-  /home/milenap/Downloads/omnetpp-6.1/include/omnetpp.h
-/home/milenap/Downloads/omnetpp-6.1/include/omnetpp.h:
--- out/clang-release/Queue.o.d
+++
@@ -1,3 +0,0 @@
-out/clang-release//Queue.o: Queue.cc \
-  /home/milenap/Downloads/omnetpp-6.1/include/omnetpp.h
-/home/milenap/Downloads/omnetpp-6.1/include/omnetpp.h:
