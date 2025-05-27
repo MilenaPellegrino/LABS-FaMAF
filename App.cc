6ca14dcf0c4df92e52b0c66ea9acb67350e409c3
Diego <diego.guerrero@mi.unc.edu.ar>
2025-05-27 03:45:57 -0300

    implementacion parcial la ruta mas rapida

--- App.cc
+++ App.cc
@@ -12,7 +12,6 @@ private:
     cMessage *sendMsgEvent;
     cStdDev delayStats;
     cOutVector delayVector;
-    cStdDev jumpsStats;
 public:
     App();
     virtual ~App();
@@ -44,40 +43,15 @@ void App::initialize() {
     // Initialize statistics
     delayStats.setName("TotalDelay");
     delayVector.setName("Delay");
-    jumpsStats.setName("Jumps");
-
 }
 
 void App::finish() {
     // Record statistics
     recordScalar("Average delay", delayStats.getMean());
     recordScalar("Number of packets", delayStats.getCount());
-    recordScalar("Max hops", jumpsStats.getMax());
-    recordScalar("Min hops", jumpsStats.getMin());
 }
 
 void App::handleMessage(cMessage *msg) {
-
-    // Paquete de testeo 
-    if (canSendTestPacket()){
-        // Creamos el paquete de testeo
-        // Al packet hay que pasarle un kind, por ahora le puse -1, para que sea diferente a todos, pero no se si es lo correcto 
-        Packet *pkt = new Packet("testeo", -1); 
-        pkt->setByteLength(par("packetTesteoByteSize"));  // Capaz le erre con el byte size (ver en .ned)
-        pkt->setSource(this->getParentModule()->getIndex());  // El source es para indicar que nodo creo el paquete
-        pkt->setDestination(par("destination"));  // Que nodo es el destino del paquete
-        pkt->addPar("Jumps") = 0;  // Como recien creamos un paquete, la cantidad de nodos por lo que paso es 0.
-
-        // Hay un campo en packet que es hopCount  pero por lo que lei en el codigo no lo usa nunca. 
-        // Yo creo que en vez de pkt->addPar("Jumps") = 0;  que por lo que entendi lo que hace es crear una variable nueva al packet en tiempo de ejecucion
-        // Tendriamos que setear el campo de hopCount, pero lo deje con el addPar ya que es lo que hace el codigo dado por la catedra. PREGUNTAR 
-
-        // send to net layer
-        send(pkt, "toNet$o");
-
-
-        // FALTA EL SCHEDULE (capaz que haya que poner otro tiempo diferente, es el "x" de nuestra implementacion)
-    }
 
     // if msg is a sendMsgEvent, create and send new packet
     if (msg == sendMsgEvent) {
@@ -86,7 +60,7 @@ void App::handleMessage(cMessage *msg) {
         pkt->setByteLength(par("packetByteSize"));
         pkt->setSource(this->getParentModule()->getIndex());
         pkt->setDestination(par("destination"));
-        pkt->addPar("Jumps") = 0;
+        pkt->setPacketType(0);
 
         // send to net layer
         send(pkt, "toNet$o");
@@ -100,10 +74,8 @@ void App::handleMessage(cMessage *msg) {
     else {
         // compute delay and record statistics
         simtime_t delay = simTime() - msg->getCreationTime();
-        long jumps = msg->par("Jumps").longValue();
         delayStats.collect(delay);
         delayVector.record(delay);
-        jumpsStats.collect(jumps);
         // delete msg
         delete (msg);
     }
--- Net.cc
+++ Net.cc
@@ -9,15 +9,24 @@ using namespace omnetpp;
 
 class Net: public cSimpleModule {
 private:
-    cOutVector chargeVector;
+
 public:
     Net();
     virtual ~Net();
+    struct RouteInfo {
+        int bestInterface = -1;
+        simtime_t lastDelay = SIMTIME_ZERO;
+        int packetsSent = 0;
+        int feedbacksReceived = 0;
+    };
+    std::map<std::pair<int, int>, RouteInfo> routingTable;
+    std::set<std::pair<int, int>> testSent;
 protected:
     virtual void initialize();
     virtual void finish();
     virtual void handleMessage(cMessage *msg);
 };
+
 
 Define_Module(Net);
 
@@ -30,31 +39,135 @@ Net::~Net() {
 }
 
 void Net::initialize() {
-    chargeVector.setName("charge");
+    
 }
 
 void Net::finish() {
-    recordScalar("charge", chargeVector.getValuesReceived());
 }
 
 void Net::handleMessage(cMessage *msg) {
 
     // All msg (events) on net are packets
+    
     Packet *pkt = (Packet *) msg;
-    chargeVector.record(1);
+    //total de nodos
+    int numHosts = getParentModule()->getVectorSize();
+    //nodo actual
+    int myIndex = this->getParentModule()->getIndex();
+    //destino
+    int dest = pkt->getDestination();
+    //origen
+    int origin = pkt->getSource();
+    //tipo de paquete
+    int type = pkt->getPacketType();
+ 
+    if (type == 2) {//el paquete es un feedback
+        if (dest == myIndex) {
+            //llego a su destino
+            std::pair<int, int> key = std::make_pair(myIndex, origin);
+            int arrivedGate = pkt->getArrivalGate()->getIndex();
+            simtime_t delay = simTime() - pkt->getCreationTime(); // Delay estimado
+        
+            // Si no había info, o la nueva es mejor, actualizamos
+            if (routingTable.find(key) == routingTable.end() || delay < routingTable[key].lastDelay) {
+                routingTable[key].bestInterface = arrivedGate;
+                routingTable[key].lastDelay = delay;
+            }
+            delete pkt; // ya no se necesita
 
-    // If this node is the final destination, send to App
-    if (pkt->getDestination() == this->getParentModule()->getIndex()) {
+            } else {
+                // reenviar el feedback hacia su origen
+                int arrivedGate = pkt->getArrivalGate()->getIndex();
+                // enviamos por el lado opuesto
+                int returnIface;
+                if (arrivedGate == 0){
+                    returnIface = 1;
+                }else{
+                    returnIface = 0; 
+                }
+                send(pkt, "toLnk$o", returnIface);
+            }
+               
+
+    }else if(type == 1){//el paquete es un test
+        if (dest == myIndex) {
+            // el test llega al destino
+            // Creamos un paquete de feedback para retornar
+            Packet *feedback = pkt->dup();
+            feedback->setPacketType(2);
+            //se lo mandamos de vuelta al origen
+            feedback->setDestination(origin);
+            feedback->setSource(myIndex);
+        
+            // Tomamos la interfaz por donde vino este test
+            int arrivedGate = pkt->getArrivalGate()->getIndex();
+            
+            send(feedback, "toLnk$o", arrivedGate);
+            // eliminamos el paquete de test que ya es inecesario
+            delete pkt; 
+        } else {
+            // reenviamos el test en la misma dirección
+            int arrivedGate = pkt->getArrivalGate()->getIndex();
+            // enviamos por el lado opuesto
+             int returnIface;
+            if (arrivedGate == 0){
+                returnIface = 1;
+            }else{
+                returnIface = 0; 
+            }
+            send(pkt, "toLnk$o", returnIface);
+        }
+    
+    }else if(dest == myIndex){//el paquete es original y esta en el final
         send(msg, "toApp$o");
-    }
-    // If not, forward the packet to some else... to who?
-    else {
-        long jumps = msg->par("Jumps").longValue();
-        jumps++;
-        // We send to link interface #0, which is the
-        // one connected to the clockwise side of the ring
-        // Is this the best choice? are there others?
-        msg->par("Jumps").setLongValue(jumps);
-        send(msg, "toLnk$o", 0);
+    }else if(origin == myIndex){//es el paquete original y esta al principio
+        // Creamos una clave única para la tabla de ruteo: (origen, destino)
+        std::pair<int, int> key = std::make_pair(origin, dest);
+        // Verificamos si ya tenemos una ruta registrada
+        if (routingTable.find(key) != routingTable.end()) {
+            // Ruta conocida: reenviar por la mejor interfaz registrada
+            int bestIface = routingTable[key].bestInterface;
+
+            // por si el valor no es válido
+            
+            if (bestIface >= 0 && bestIface < gateSize("toLnk$o")) {
+                send(msg, "toLnk$o", bestIface);
+            } else {
+            // Si por alguna razón el valor es inválido, reenviar por interfaz por defecto (0)
+            send(msg, "toLnk$o", 0);
+            }
+        } else {
+            // no hay ruta optima actualmente y estamos en el lugar de origen del paquete
+            if (testSent.find(key) == testSent.end()){ //se hizo el primer test?
+                //si no
+                //Marcamos que se hizo el testeo
+                testSent.insert(key);
+                //duplicamos el paquete original
+                Packet *test1 = pkt->dup();
+                Packet *test2 = pkt->dup();
+                //cambiamos el tipo de a test
+                test1->setPacketType(1);
+                test2->setPacketType(1);
+                //lo enviamos por ambos lados
+                send(test1, "toLnk$o", 0); // horario
+                send(test2, "toLnk$o", 1); // antihorario
+                //enviamos el original
+                send(pkt, "toLnk$o", 0);
+            }else{
+                //si ya se hizo el test pero aun no se tiene ruta se envia por defecto
+                send(msg, "toLnk$o", 0);
+            }
+        }
+    }else{//es el paquete original y no esta al principio ni al final
+        int arrivedGate = pkt->getArrivalGate()->getIndex();
+        // enviamos por el lado opuesto 
+        //(hay muchos de estos ya que al principio me salio la cosa al reves)
+        int returnIface;
+        if (arrivedGate == 0){
+            returnIface = 1;
+        }else{
+            returnIface = 0; 
+        }
+        send(pkt, "toLnk$o", returnIface);
     }
 }
