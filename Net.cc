f94d9d819b75394f67dafa2aa9f5344603755bee
MilenaPellegrino <milenapellegrino@mi.unc.edu.ar>
2025-05-27 21:28:24 -0300

    feature(Net): Completado tarea de diseno

--- Net.cc
+++ Net.cc
@@ -1,30 +1,35 @@
 #ifndef NET
 #define NET
 
+const int TEST = 1;
+const int FEEDBACK = 2;
+
+const int HORARIA = 0;
+const int ANTIHORARIA = 1;
+
 #include <string.h>
 #include <omnetpp.h>
 #include <packet_m.h>
 
 using namespace omnetpp;
+using namespace std;
 
 class Net: public cSimpleModule {
 private:
-
+    int currentInterface = HORARIA;
+    map<int, bool> arrivalPackage;  // Dado un origin nos devuelve si ya llego el primer paquete de testeo
+    bool sendPackage = true;
+    int sendedPackage = 0;
 public:
     Net();
     virtual ~Net();
-    struct RouteInfo {
-        int bestInterface = -1;
-        simtime_t lastDelay = SIMTIME_ZERO;
-        int packetsSent = 0;
-        int feedbacksReceived = 0;
-    };
-    std::map<std::pair<int, int>, RouteInfo> routingTable;
-    std::set<std::pair<int, int>> testSent;
 protected:
     virtual void initialize();
     virtual void finish();
     virtual void handleMessage(cMessage *msg);
+    virtual void routing(cMessage *msg);
+    virtual void crearFeedback(cMessage *msg);
+    virtual int gateOpuesto(cMessage *msg);
 };
 
 
@@ -45,11 +50,84 @@ void Net::initialize() {
 void Net::finish() {
 }
 
-void Net::handleMessage(cMessage *msg) {
-
-    // All msg (events) on net are packets
-    
+void Net::routing(cMessage *msg){
     Packet *pkt = (Packet *) msg;
+
+    // creamos paquetes de testeo
+    Packet *test0 = new Packet("testPacket0", this->getParentModule()->getIndex());
+    Packet *test1 = new Packet("testPacket1", this->getParentModule()->getIndex());
+
+    test0->setPacketType(TEST);
+    test1->setPacketType(TEST);
+
+    test0->setSource(this->getParentModule()->getIndex());
+    test1->setSource(this->getParentModule()->getIndex());
+
+    test0->setDestination(pkt->getDestination());
+    test1->setDestination(pkt->getDestination());
+
+    test0->setByteLength(pkt->getByteLength());
+    test1->setByteLength(pkt->getByteLength());
+
+    
+    // Enviamos los paquetes de testeo 
+    send(test0, "toLnk$o", 0); // horario
+    send(test1, "toLnk$o", 1); // antihorario
+}
+
+void Net::crearFeedback(cMessage *msg){
+
+    Packet *pkt = (Packet *) msg;
+
+    // creamos paquetes de feedback
+    Packet *feedback = new Packet("feedback", this->getParentModule()->getIndex());
+
+    feedback->setPacketType(FEEDBACK);
+
+    // El origin del feedback es el destino del paquete testeo
+    feedback->setSource(pkt->getDestination());
+
+    // El destino del feedback es el origen del paquete de testeo
+    feedback->setDestination(pkt->getSource());
+
+    feedback->setByteLength(pkt->getByteLength());
+    
+    // Enviamos los paquetes de testeo por la ruta optima elegida
+    send(feedback, "toLnk$o", pkt->getArrivalGate()->getIndex());
+}
+
+int Net::gateOpuesto(cMessage *msg){
+    Packet *pkt = (Packet *) msg;
+    int arrivedGate = pkt->getArrivalGate()->getIndex();
+    // enviamos por el lado opuesto
+    int interfazOpuesta;
+    if(arrivedGate == HORARIA){
+        interfazOpuesta = ANTIHORARIA;
+    }else{
+        interfazOpuesta = HORARIA;
+    }
+    return interfazOpuesta;
+}
+
+void Net::handleMessage(cMessage *msg) {
+    // All msg (events) on net are packets
+    Packet *pkt = (Packet *) msg;
+
+    cGate * arrivalGate = pkt->getArrivalGate();
+    if(arrivalGate != nullptr){
+        const char * gateName = arrivalGate->getName();
+        if(strcmp(gateName, "toApp$i") == 0){
+            if(sendPackage){
+                routing(msg);
+                sendPackage = false;
+                sendedPackage = 0;
+            } 
+            sendedPackage++;
+            sendPackage = sendedPackage > 15;
+        }
+
+    }
+
     //total de nodos
     int numHosts = getParentModule()->getVectorSize();
     //nodo actual
@@ -58,121 +136,47 @@ void Net::handleMessage(cMessage *msg) {
     int dest = pkt->getDestination();
     //origen
     int origin = pkt->getSource();
+
     //tipo de paquete
     int type = pkt->getPacketType();
- 
-    if (type == 2) {//el paquete es un feedback
+
+    if (type == FEEDBACK) {
         if (dest == myIndex) {
-            //llego a su destino
-            std::pair<int, int> key = std::make_pair(myIndex, origin);
-            int arrivedGate = pkt->getArrivalGate()->getIndex();
-            simtime_t delay = simTime() - pkt->getCreationTime(); // Delay estimado
-        
-            // Si no había info, o la nueva es mejor, actualizamos
-            if (routingTable.find(key) == routingTable.end() || delay < routingTable[key].lastDelay) {
-                routingTable[key].bestInterface = arrivedGate;
-                routingTable[key].lastDelay = delay;
-            }
+            // Llego a su destino
+            currentInterface = pkt->getArrivalGate()->getIndex();
             delete pkt; // ya no se necesita
+        } else {  // No llego a destino 
+            send(pkt, "toLnk$o",   gateOpuesto(msg));
+        }          
+    } else if(type == TEST){
+        // Si el test no llego a destino lo seguimos mandando por esa ruta
+        if(dest!= myIndex){send(pkt, "toLnk$o", gateOpuesto(msg));}
 
-            } else {
-                // reenviar el feedback hacia su origen
-                int arrivedGate = pkt->getArrivalGate()->getIndex();
-                // enviamos por el lado opuesto
-                int returnIface;
-                if (arrivedGate == 0){
-                    returnIface = 1;
-                }else{
-                    returnIface = 0; 
-                }
-                send(pkt, "toLnk$o", returnIface);
+        // Si el test llego a destino
+        if(dest==myIndex){
+            auto it = arrivalPackage.find(origin);
+            if(it == arrivalPackage.end() || !(it->second)){
+                arrivalPackage[origin] = true;
+                currentInterface = gateOpuesto(msg);
+                crearFeedback(msg);
+            } else{
+                arrivalPackage[origin] = false;
             }
-               
-
-    }else if(type == 1){//el paquete es un test
-        if (dest == myIndex) {
-            // el test llega al destino
-            // Creamos un paquete de feedback para retornar
-            Packet *feedback = pkt->dup();
-            feedback->setPacketType(2);
-            //se lo mandamos de vuelta al origen
-            feedback->setDestination(origin);
-            feedback->setSource(myIndex);
-        
-            // Tomamos la interfaz por donde vino este test
-            int arrivedGate = pkt->getArrivalGate()->getIndex();
             
-            send(feedback, "toLnk$o", arrivedGate);
-            // eliminamos el paquete de test que ya es inecesario
-            delete pkt; 
-        } else {
-            // reenviamos el test en la misma dirección
-            int arrivedGate = pkt->getArrivalGate()->getIndex();
-            // enviamos por el lado opuesto
-             int returnIface;
-            if (arrivedGate == 0){
-                returnIface = 1;
-            }else{
-                returnIface = 0; 
-            }
-            send(pkt, "toLnk$o", returnIface);
+            delete pkt;
         }
-    
-    }else if(dest == myIndex){//el paquete es original y esta en el final
-        send(msg, "toApp$o");
-    }else if(origin == myIndex){//es el paquete original y esta al principio
-        // Creamos una clave única para la tabla de ruteo: (origen, destino)
-        std::pair<int, int> key = std::make_pair(origin, dest);
-        // Verificamos si ya tenemos una ruta registrada
-        if (routingTable.find(key) != routingTable.end()) {
-            // Ruta conocida: reenviar por la mejor interfaz registrada
-            int bestIface = routingTable[key].bestInterface;
 
-            // por si el valor no es válido
-            
-            if (bestIface >= 0 && bestIface < gateSize("toLnk$o")) {
-                send(msg, "toLnk$o", bestIface);
-            } else {
-            // Si por alguna razón el valor es inválido, reenviar por interfaz por defecto (0)
-            send(msg, "toLnk$o", 0);
-            }
-        } else {
-            // no hay ruta optima actualmente y estamos en el lugar de origen del paquete
-            if (testSent.find(key) == testSent.end()){ //se hizo el primer test?
-                //si no
-                //Marcamos que se hizo el testeo
-                testSent.insert(key);
-                //creamos paquetes de testeo
-                Packet *test1 = new Packet("testPacket1");
-                Packet *test2 = new Packet("testPacket2");
-                test1->setPacketType(1);
-                test2->setPacketType(1);
-                test1->setSource(myIndex);
-                test2->setSource(myIndex);
-                test1->setDestination(dest);
-                test2->setDestination(dest);
-                test1->setByteLength(128);
-                test2->setByteLength(128);
-                //lo enviamos por ambos lados
-                send(test1, "toLnk$o", 0); // horario
-                send(test2, "toLnk$o", 1); // antihorario
-                //enviamos el original
-                send(pkt, "toLnk$o", 0);
-            }else{
-                //si ya se hizo el test pero aun no se tiene ruta se envia por defecto
-                send(msg, "toLnk$o", 0);
-            }
+    }else{  // El paquete es "normal" 
+        if(dest == myIndex){ // Esta en el destino
+            send(pkt, "toApp$o");
+        } else if (origin == myIndex){  // Esta al principio 
+            send(pkt, "toLnk$o", currentInterface);
+        } else {  // Esta en un nodo intermedio 
+            send(pkt, "toLnk$o",   gateOpuesto(msg));
         }
-    }else{//es el paquete original y no esta al principio ni al final
-        int arrivedGate = pkt->getArrivalGate()->getIndex();
-        // enviamos por el lado opuesto 
-        //(hay muchos de estos ya que al principio me salio la cosa al reves)
-        int returnIface;
-        if (arrivedGate == 0){
-            returnIface = 1;
-        }else{
-            returnIface = 0; 
-        }
-        send(pkt, "toLnk$o", returnIface);
     }
-}
+
+}   
+    
+
+    
